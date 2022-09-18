56263ab9fd8ec68ca7d5e8eebb6f8280219b2623
Arseniy Suschevskiy <a.suschevskiy@voiso.com>
2022-09-18 14:46:29 +0300

    fix where mode

--- backend/src/routes/person/index.ts
+++ backend/src/routes/person/index.ts
@@ -15,7 +15,7 @@ const personRoutes: FastifyPluginAsync = async (fastify, opts): Promise<void> =>
     return fastify.prisma.person.findMany({
       take: query.take || 100,
       skip: query.skip,
-      where: { name: { contains: query.title } }
+      where: { name: { contains: query.title, mode: "insensitive" } }
     })
   })
 }
